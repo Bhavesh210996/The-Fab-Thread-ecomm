a749e1abf0d77230da6c980ce3413a6bfa11919c
Bhavesh Bafana <bhaveshbafana96@gmail.com>
2025-02-14 15:35:25 +0530

    metadata changes

--- src/components/ui/UserProfile.js
+++ src/components/ui/UserProfile.js
@@ -24,7 +24,7 @@ function UserProfile({onLogout, userLoggingOut}) {
             <NavLink onClick={() => onLogout()} className="drpdwn-link logout">
               <HiOutlineArrowRightOnRectangle />
               <span>Logout</span>
-              {!userLoggingOut && <SpinnerMini />}
+              {userLoggingOut && <SpinnerMini />}
             </NavLink>
           )}
         </div>
